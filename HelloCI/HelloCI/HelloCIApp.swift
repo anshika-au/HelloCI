9b5ca471350cd299e6317f0ffa691c47a047a7ac
anshikakalra <kalra.anshika.au@gmail.com>
2026-02-18 20:30:25 +1100

    Initial setup

---
+++ HelloCI/HelloCI/HelloCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCIApp.swift
+//  HelloCI
+//
+//  Created by anshika kalra on 18/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct HelloCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloCI/HelloCITests/HelloCITests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCITests.swift
+//  HelloCITests
+//
+//  Created by anshika kalra on 18/2/2026.
+//
+
+import Testing
+@testable import HelloCI
+
+struct HelloCITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
